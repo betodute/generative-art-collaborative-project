d4e90019be4cbef5c816d7ab55da540009e49505
Alberto Duarte <betodute@gmail.com>
2023-01-11 20:32:06 -0800

    full functionality working

--- CircleBackground.js
+++ CircleBackground.js
@@ -1,7 +1,7 @@
 class CircleBackground {
   constructor() {
-    this.x = random(windowWidth);
-    this.y = random(windowHeight);
+    this.x = random(700);
+    this.y = random(450);
     this.r = random(255);
     this.g = random(255);
     this.b = random(255);
@@ -16,21 +16,18 @@ class CircleBackground {
     graphicsLayer.noStroke();
 
     this.circleButton.mousePressed(() => {
-      graphicsLayer.frameRate(10);
-      graphicsLayer.fill(this.r, this.g, this.b, 100);
       graphicsLayer.circle(this.x, this.y, 200)
+      graphicsLayer.fill(this.r, this.g, this.b, 100);
     });
 
     this.triangleButton.mousePressed(() => {
-      graphicsLayer.frameRate(10);
+      graphicsLayer.triangle(this.x - 100, this.y, this.x + 100, this.y - 100, this.x + 200, this.y)
       graphicsLayer.fill(this.r, this.g, this.b, 100);
-      graphicsLayer.triangle(this.x, this.y, this.x + 100, this.y - 100, this.x + 50, this.y - 150)
     });
     
     this.squareButton.mousePressed(() => {
-      graphicsLayer.frameRate(10);
+      graphicsLayer.square(this.x - 100, this.y - 100, 200);
       graphicsLayer.fill(this.r, this.g, this.b, 100);
-      graphicsLayer.square(this.x, this.y, 200);
     })
 
   };
