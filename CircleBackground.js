abd20fe418ce898e590540819c647e044bae4010
Alberto Duarte <betodute@gmail.com>
2023-01-11 18:41:41 -0800

    button response and structure up still need hex

--- CircleBackground.js
+++ CircleBackground.js
@@ -5,13 +5,27 @@ class CircleBackground {
     this.r = random(255);
     this.g = random(255);
     this.b = random(255);
+    this.circleButton = select("#circle-button");
+    this.triangleButton = select("#triangle-button");
+    this.hexButton = select("#hex-button");
   };
 
   render() {
-    graphicsLayer.frameRate(50);
+    graphicsLayer.frameRate(10);
     graphicsLayer.noStroke();
     graphicsLayer.fill(this.r, this.g, this.b, 100);
-    graphicsLayer.circle(this.x, this.y, 100);
+    graphicsLayer.circle(this.x, this.y, 300);
+
+    this.circleButton.mousePressed(() => {
+      graphicsLayer.circle(this.x, this.y, 300)
+    });
+
+    this.triangleButton.mousePressed(() => {
+      graphicsLayer.triangle(this.x, this.y, this.x + 25, this.y - 25, this.x + 5, this.y - 25)
+    });
+    
+
   };
+
 
 };
