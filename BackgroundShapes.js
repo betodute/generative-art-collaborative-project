da98af5b64a044edf5e0e0407afa5f7c5bcb1fe0
Matthew Sanner <gravityskunk@gmail.com>
2023-01-15 18:57:15 -0600

    fixed canvas shape generation

--- BackgroundShapes.js
+++ BackgroundShapes.js
@@ -1,33 +1,30 @@
-class CircleBackground {
-  constructor(selectedShape) {
+class BackgroundShapes {
+  constructor(backgroundShape) {
     this.x = random(700);
-    this.y = random(450);
+    this.y = random(700);
     this.r = random(255);
     this.g = random(255);
     this.b = random(255);
-    this.selectedShape = selectedShape;
+    this.backgroundShape = backgroundShape;
   };
 
   render() {
-    
     graphicsLayer.noStroke();
 
-    if (this.selectedShape === "circle") {
+    if (backgroundShape === "circle") {
       graphicsLayer.circle(this.x, this.y, 200);
       graphicsLayer.fill(this.r, this.g, this.b, 100);
     };
 
-    if (this.selectedShape === "triangle") {
+    if (backgroundShape === "triangle") {
       graphicsLayer.triangle(this.x - 100, this.y, this.x + 100, this.y - 100, this.x + 200, this.y)
       graphicsLayer.fill(this.r, this.g, this.b, 100);
     };
 
-    if (this.selectedShape === "square") {
+    if (backgroundShape === "square") {
       graphicsLayer.square(this.x - 100, this.y - 100, 200);
       graphicsLayer.fill(this.r, this.g, this.b, 100);
     }
 
   };
-
-
 };
---
+++ ForegroundShapes.js
@@ -0,0 +1,52 @@
+class ForegroundShapes {
+    constructor(foregroundShape) {
+        this.ang1 = TWO_PI * noise(0.01 * frameCount + 10) /*+ mouseX / 100*/;
+        this.ang2 = TWO_PI * noise(0.01 * frameCount + 20) /*+ mouseY / 100*/;
+        this.ang3 = TWO_PI * noise(0.01 * frameCount + 30) /*+ (mouseX + mouseY) / 100*/;
+        this.rx = 60 * noise(0.01 * frameCount + 40) + mouseX / 7;
+        this.tx = 200 * noise(0.01 * frameCount + 50) + mouseY / 7;
+        this.size1 = 300 * noise(0.01 * frameCount + 60);
+        this.size2 = 100 * noise(0.01 * frameCount + 60);
+        this.foregroundShape = foregroundShape;
+    }
+
+    render() {
+        frameRate(30);
+        rectMode(CENTER);
+        noStroke();
+        fill(0, 0, 0, 255);
+        translate(width / 2, height / 2);
+        for (var i = 0; i < 8; i++) {
+            push();
+            rotate(this.ang1 + TWO_PI * i / 8);
+            translate(this.tx, 0);
+            if (foregroundShape === 'circle') {
+                circle(0, 0, this.size1);
+            }
+            if (foregroundShape === 'triangle') {
+                triangle(0, 0, this.rx, this.rx, this.tx, this.tx);
+            }
+            if (foregroundShape === 'square') {
+                rect(0, 0, this.size1, this.size1);
+            }
+            for (var j = 0; j < 6; j++) {
+                push();
+                rotate(this.ang2 + TWO_PI * j / 6);
+                translate(this.rx, 0);
+                rotate(this.ang3);
+                if (foregroundShape === 'circle') {
+                    circle(this.rx, 0, this.size2);
+                }
+                if (foregroundShape === 'triangle') {
+                    triangle(this.size1, this.size2, this.rx, this.rx, this.tx, this.tx);
+                }
+                if (foregroundShape === 'square') {
+                    rect(this.rx, 0, this.size2, this.size2);
+                }
+                pop();
+            }
+            translate()
+            pop();
+        }
+    }
+}
