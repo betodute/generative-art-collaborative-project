75f6591ccf7713814c72b7453535b47e76d698b3
Alberto Duarte <betodute@gmail.com>
2023-01-18 20:33:13 -0800

    Color Tones On POINT

--- BackgroundShapes.js
+++ BackgroundShapes.js
@@ -2,12 +2,19 @@ class BackgroundShapes {
   constructor(backgroundShape) {
     this.x = random(700);
     this.y = random(700);
-    this.r = random(255);
-    this.g = random(255);
-    this.b = random(255);
-    this.blueToneR = random(0, 2);
-    this.blueToneG = random(0, 25);
-    this.blueToneB = random(100, 200);
+    
+    this.blueToneR = random(0, 125);
+    this.blueToneG = random(0, 125);
+    this.blueToneB = random(100, 255);
+
+    this.greenToneR = random(25, 180);
+    this.greenToneG = random(75, 255);
+    this.greenToneB = random(25, 240);
+
+    this.yellowToneR = random(150, 255);
+    this.yellowToneG = random(150, 255);
+    this.yellowToneB = random(0, 200);
+
     this.backgroundShape = backgroundShape;
   };
 
@@ -15,18 +22,18 @@ class BackgroundShapes {
     graphicsLayer.noStroke();
 
     if (backgroundShape === "circle") {
-      graphicsLayer.circle(this.x, this.y, 200);
+      graphicsLayer.circle(this.x, this.y, 150);
       graphicsLayer.fill(this.blueToneR, this.blueToneG, this.blueToneB, 100);
     };
 
     if (backgroundShape === "triangle") {
-      graphicsLayer.triangle(this.x - 100, this.y, this.x + 100, this.y - 100, this.x + 200, this.y)
-      graphicsLayer.fill(this.r, this.g, this.b, 100);
+      graphicsLayer.triangle(this.x - 125, this.y - 50, this.x - 25, this.y + 75, this.x + 75, this.y - 50)
+      graphicsLayer.fill(this.greenToneR, this.greenToneG, this.greenToneB, 100);
     };
 
     if (backgroundShape === "square") {
-      graphicsLayer.square(this.x - 100, this.y - 100, 200);
-      graphicsLayer.fill(this.r, this.g, this.b, 100);
+      graphicsLayer.square(this.x - 100, this.y - 100, 150);
+      graphicsLayer.fill(this.yellowToneR, this.yellowToneG, this.yellowToneB, 100);
     }
 
   };
